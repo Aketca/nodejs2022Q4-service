ce741d6786d941d08625b9390379b748425e8a04
Aketca <Someone9379992@gmail.com>
2023-02-01 19:32:24 +0200

    save crud in process

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+// import { useContainer } from 'class-validator';
+import { ValidationPipe } from '@nestjs/common';
+
 dotenv.config();
 const port = process.env.PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist: true,
+      transform: true,
+    }),
+  );
+  // useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(port || 4000);
 }
 bootstrap();
---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [];
+  create(createUserDto: CreateUserDto) {
+    const user = {
+      id: uuidv4(),
+      ...createUserDto,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      version: 1,
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((item) => item.id === id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
